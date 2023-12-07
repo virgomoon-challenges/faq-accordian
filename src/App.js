c76078032a75443f6d5ea2ae4d78e3abb9df3d1b
Joseph Guillory <jagmanx99@yahoo.com>
2023-12-07 14:18:07 -0800

    Add SVG

--- src/App.js
+++ src/App.js
@@ -1,13 +1,16 @@
-// import logo from './logo.svg';
 import './App.css';
+import deskTopHeader from './assets/background-pattern-desktop.svg';
+import { ReactComponent as Star } from './assets/icon-star.svg';
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
+        <img src={deskTopHeader} alt="" />
         <a>
           Learn React
         </a>
+        <Star />
       </header>
     </div>
   );
